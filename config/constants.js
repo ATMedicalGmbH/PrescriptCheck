69c8563146c6fc3373de0ede3a5c7c98fcb7c202
Andreas Tremml <Andreas.Tremml@at-medical.de>
2025-05-17 21:00:36 +0200

    Add files via upload

---
+++ config/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ROLE_ADMIN: 'admin',
+  ROLE_DOCTOR: 'doctor',
+  ROLE_PHARMACIST: 'pharmacist',
+  SECURITY_LEVEL_0: 0,
+  SECURITY_LEVEL_1: 1,
+  SECURITY_LEVEL_2: 2,
+  SECURITY_LEVEL_3: 3,
+  TOKEN_EXPIRATION: '1h',
+};
